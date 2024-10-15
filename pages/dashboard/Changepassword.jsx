2834715da24be230c10d56fe7ed18d6a9fec4116
harshtrivedi01 <harsh2trivedi786@gmail.com>
2024-10-15 18:06:40 +0530

    update

--- pages/dashboard/Changepassword.jsx
+++ pages/dashboard/Changepassword.jsx
@@ -1,7 +1,8 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
-import toast, { ToastContainer } from "react-toastify";
+import toast from "react-toastify";
+import { ToastContainer } from "react-toastify";
 
 function Changepassword() {
   const [showOldPassword, setShowOldPassword] = useState(false);
