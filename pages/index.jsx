0095462f1993c32d3487898af989c294604073a9
harshtrivedi01 <harsh2trivedi786@gmail.com>
2024-10-15 15:37:43 +0530

    fds

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,7 +2,7 @@ import React, {  } from "react";
 import Builder from "./builder";
 import Loder from './Loder'
 import Home_first from "./Home/Home_first";
-import FAQ from "./Home/FAQ/FAQ_Component";
+import FAQ from "./Home/FAQ/FAQ_Component.jsx";
 import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 
