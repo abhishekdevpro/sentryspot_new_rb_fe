43e92c1e0ec770eac0604216b0ec7da8b16ac9b0
Rishi kashyap <kashyap1strishi@gmail.com>
2024-11-28 16:50:36 +0530

    Token set added

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,4 +1,4 @@
-import React, {  } from "react";
+import React, { useEffect, useState } from "react";
 import Builder from "./builder";
 import Loder from './Loder'
 import Home_first from "./Home/Home_first";
@@ -7,6 +7,26 @@ import Navbar from "./Navbar/Navbar";
 import Footer from "./Footer/Footer";
 
 const ResumeBuilder = () => {
+  const [token, setToken] = useState(null);
+  useEffect(() => {
+    // Extract the token directly from the URL
+    const url = window.location.href;
+    const tokenFromUrl = url.split("/?")[1]; // Gets the token part after `/?`
+
+    if (tokenFromUrl) {
+      // Save token to localStorage and state
+      localStorage.setItem("token", tokenFromUrl);
+      setToken(tokenFromUrl);
+    } else if (typeof window !== "undefined") {
+      // Retrieve token from localStorage if not found in URL
+      const storedToken = localStorage.getItem("token");
+      setToken(storedToken);
+    }
+  }, []);
+
+  if (!token) {
+    return null; // Exit if no token
+  }
   return (
     <>
     {/* <Builder /> */}
