fcde0dc4f3ac2c4766a687880e72a0b0a688aaaf
Anita Chouhan <fe.dev2.ba2024@gmail.com>
2025-01-15 14:06:31 +0530

    resumelist , previewsection ,templates8to12updated

--- components/preview/ContactAndSocial.jsx
+++ components/preview/ContactAndSocial.jsx
@@ -274,7 +274,7 @@ const ContactAndSocialMedia = ({
         ))
       ) : (
         <p className={`text-gray-600 dark:text-gray-300 ${textColor}`}>
-          No social media links available
+        
         </p>
       )}
     </div>
